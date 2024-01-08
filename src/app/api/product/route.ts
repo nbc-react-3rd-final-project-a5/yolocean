a50beb0a6334564925fa3900d47a08e918c9cb2e
peeChulchul <eyeless356@naver.com>
2024-01-08 13:57:00 +0900

    add:상품 읽어오는 api라우터생성

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,5 +1,11 @@
+import { supabase } from "@/service/supabase";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function GET() {
-  return NextResponse.json("상품 GET");
-}
+export const GET = async () => {
+  let { data: post, error } = await supabase.from("product").select("*");
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+  return NextResponse.json(post);
+};
