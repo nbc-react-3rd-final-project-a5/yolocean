83e6fabf9b4478936528afcff9f8eea912c0d78b
Majinwoo <maddonge@naver.com>
2024-01-08 22:57:55 +0900

    update: react hook form을 사용한 input 태그 생성

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+"use client";
+import { Product } from "@/types/db";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-const page = () => {
-  return <div>page</div>;
+type ProductForm = Omit<Product, "id" | "view">;
+
+const ProductForm = () => {
+  const defaultValues = {
+    name: "",
+    price: 0,
+    thumbnail: "",
+    info: "",
+    original_price: 0,
+    info_img: "",
+    category_id: ""
+  };
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm<ProductForm>({
+    mode: "onSubmit",
+    defaultValues: defaultValues
+  });
+
+  const handleProductFormSubmit = (data: ProductForm) => {
+    alert(data);
+  };
+
+  return (
+    <form
+      className="w-[300px] flex flex-col justify-center align-center"
+      onSubmit={handleSubmit(handleProductFormSubmit)}
+    >
+      <input className=" border-black border-solid border" id="name" type="text" {...register("name")} />
+      <input className=" border-black border-solid border" id="price" type="text" {...register("price")} />
+      <input className=" border-black border-solid border" id="thumbnail" type="text" {...register("thumbnail")} />
+      <input className=" border-black border-solid border" id="info" type="text" {...register("info")} />
+      <input
+        className=" border-black border-solid border"
+        id="original_price"
+        type="text"
+        {...register("original_price")}
+      />
+      <input className=" border-black border-solid border" id="info_img" type="text" {...register("info_img")} />
+      <input className=" border-black border-solid border" id="category_id" type="text" {...register("category_id")} />
+      <button type="submit">제출</button>
+    </form>
+  );
 };
 
-export default page;
+export default ProductForm;
---
+++ src/app/store/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const StoreForm = () => {
+  return <div>StoreForm</div>;
+};
+
+export default StoreForm;
