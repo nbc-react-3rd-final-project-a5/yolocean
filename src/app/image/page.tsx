daa2cc40399f72d413ba0ed1976ee72be628a9c5
dosion9 <dosion9ood@gmail.com>
2024-01-09 11:20:53 +0900

    update : 파일명 변경

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,10 +1,10 @@
-import DragAndDropImageBox from "@/components/DragAndDropImageBox";
+import DragDropImageInput from "@/components/DragDropImageInput";
 import React from "react";
 
 const page = () => {
   return (
     <div>
-      <DragAndDropImageBox />
+      <DragDropImageInput />
     </div>
   );
 };
