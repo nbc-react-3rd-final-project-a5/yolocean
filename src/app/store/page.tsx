6ed52d962231a659ecd948a9cc761e2c369b5157
Majinwoo <maddonge@naver.com>
2024-01-09 22:47:16 +0900

    update: input 기본 틀 설정 (#3)

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,9 +1,36 @@
 "use client";
 
+import { Store } from "@/types/db";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+type StoreForm = Omit<Store, "id">;
 
 const StoreForm = () => {
-  return <div>StoreForm</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue
+  } = useForm({
+    mode: "onSubmit",
+    defaultValues: {
+      name: "",
+      lat: "",
+      lng: "",
+      address: ""
+    }
+  });
+  return (
+    <input
+      className=" border-black border-solid border"
+      id="name"
+      type="text"
+      {...register("name", {
+        required: "해당 필드는 필수입니다."
+      })}
+    />
+  );
 };
 
 export default StoreForm;
