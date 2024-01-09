b6c7714d2f065a817e7e37fc2db923c9d5d95c7d
Majinwoo <maddonge@naver.com>
2024-01-09 22:45:57 +0900

    update: POST 메서드 구현 (#3)

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,5 +1,15 @@
+import { supabase } from "@/service/supabase";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET() {
-  return NextResponse.json("상품 GET");
+  //await supabaseForServer.from("books").insert({name:"foo", authorName:"bar"})
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const { error } = await supabase.from("product").insert(data);
+  if (error) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
