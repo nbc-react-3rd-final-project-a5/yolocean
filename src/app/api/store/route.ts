65921a9da90fa01a51ddcba2857c50213d9b2707
Majinwoo <maddonge@naver.com>
2024-01-09 22:47:47 +0900

    add: store api 파일 생성(#3)

---
+++ src/app/api/store/route.ts
@@ -0,0 +1,11 @@
+import { supabase } from "@/service/supabase";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET() {
+  return NextResponse.json("Store GET");
+}
+
+export async function POST(req: NextRequest) {
+  const data = req.json();
+  await supabase.from("store").insert(data);
+}
