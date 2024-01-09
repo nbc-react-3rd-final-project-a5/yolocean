d194a62938b51a3eb2782a391d447ff6b7b3a155
Majinwoo <maddonge@naver.com>
2024-01-09 15:46:00 +0900

    update: input 필드 추가

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,20 +1,19 @@
 "use client";
+import { supabase } from "@/service/supabase";
 import { Product } from "@/types/db";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
-type ProductForm = Omit<Product, "id" | "view">;
+type ProductForm = Omit<Product, "id">;
 
 const ProductForm = () => {
-  const defaultValues = {
-    name: "",
-    price: 0,
-    thumbnail: "",
-    info: "",
-    original_price: 0,
-    info_img: "",
-    category_id: ""
-  };
+  const [category, setCategory] = useState<
+    | {
+        category_name: string;
+        id: string;
+      }[]
+    | null
+  >();
 
   const {
     register,
@@ -22,22 +21,70 @@ const ProductForm = () => {
     handleSubmit
   } = useForm<ProductForm>({
     mode: "onSubmit",
-    defaultValues: defaultValues
+    defaultValues: {
+      name: "",
+      price: 0,
+      thumbnail: "",
+      original_price: 0,
+      view: 0,
+      info_img: ""
+      // category_id: ""
+    }
   });
 
-  const handleProductFormSubmit = (data: ProductForm) => {
-    alert(data);
+  const addInput = () => {
+    return (
+      <div>
+        <label>상세 정보 key 값</label>
+        <input />
+        <label>상세 정보 value 값</label>
+        <input />
+      </div>
+    );
   };
+
+  const handleProductFormSubmit = (data: ProductForm) => {
+    alert(JSON.stringify(data));
+    insertProductData(data);
+  };
+
+  const insertProductData = async (data: ProductForm) => {
+    try {
+      await supabase.from("product").insert(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchCategoryData = async () => {
+    try {
+      const { data } = await supabase.from("category").select();
+      setCategory(data);
+      console.log(data);
+    } catch (error) {
+      console.log("category fetching error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategoryData();
+  }, []);
 
   return (
     <form
       className="w-[300px] flex flex-col justify-center align-center"
       onSubmit={handleSubmit(handleProductFormSubmit)}
     >
-      <input className=" border-black border-solid border" id="name" type="text" {...register("name")} />
+      <input
+        className=" border-black border-solid border"
+        id="name"
+        type="text"
+        {...register("name", {
+          required: "해당 필드는 필수입니다."
+        })}
+      />
       <input className=" border-black border-solid border" id="price" type="text" {...register("price")} />
       <input className=" border-black border-solid border" id="thumbnail" type="text" {...register("thumbnail")} />
-      <input className=" border-black border-solid border" id="info" type="text" {...register("info")} />
       <input
         className=" border-black border-solid border"
         id="original_price"
@@ -45,7 +92,25 @@ const ProductForm = () => {
         {...register("original_price")}
       />
       <input className=" border-black border-solid border" id="info_img" type="text" {...register("info_img")} />
-      <input className=" border-black border-solid border" id="category_id" type="text" {...register("category_id")} />
+      <select {...register("category_id")}>
+        {category?.map((data) => {
+          return (
+            <option key={data.id} value={data.id}>
+              {data.category_name}
+            </option>
+          );
+        })}
+      </select>
+      <div>
+        <p>상세 정보 입력</p>
+        <label>상세 정보 key 값</label>
+        <input />
+        <label>상세 정보 value 값</label>
+        <input />
+        <button type="button" onClick={addInput}>
+          +
+        </button>
+      </div>
       <button type="submit">제출</button>
     </form>
   );
