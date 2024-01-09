6f41b92e13ff83ee5a593c716bede5b6f26b9297
yesol <cloud.mys.99@gmail.com>
2024-01-09 17:44:24 +0900

    style : 로그인 기본 ui (#6)

--- src/app/auth/SignIn.tsx
+++ src/app/auth/SignIn.tsx
@@ -69,66 +69,78 @@ const SignIn = ({ mode, setMode }: Props) => {
 
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <h2>로그인 </h2>
-          </div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">로그인 </h2>
+        </div>
 
-          <div>
-            <form>
-              <div>
-                <label htmlFor="email">Email address</label>
-                <div>
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    onChange={(e) => setId(e.target.value)}
-                    placeholder="example@yolocean.com"
-                  />
-                </div>
+        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  onChange={(e) => setId(e.target.value)}
+                  placeholder="example@yolocean.com"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-300 sm:text-sm sm:leading-6"
+                />
               </div>
+            </div>
 
-              <div>
-                <div>
-                  <label htmlFor="password">Password</label>
-                </div>
-                <div>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    onChange={(e) => setPw(e.target.value)}
-                    placeholder="비밀번호를 입력하세요."
-                  />
-                </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
               </div>
+              <div className="">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  onChange={(e) => setPw(e.target.value)}
+                  placeholder="비밀번호를 입력하세요."
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-300 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-              <div>
-                <button onClick={signInWithEmail} type="submit" disabled={isValid}>
-                  login
-                </button>
-                <br />
+            <div>
+              <button
+                onClick={signInWithEmail}
+                type="submit"
+                disabled={isValid}
+                className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 cursor-pointer"
+              >
+                login
+              </button>
+              <br />
+              <div className="grid grid-cols-2 gap-4 place-items-center">
                 <button type="button" onClick={signInWithKakao}>
-                  <img src="/images/kakao_login.png" />
+                  <img src="/images/kakao_login.png" className="h-10 w-44" />
                 </button>
-                <br />
 
                 <button type="button" onClick={signInWithGoogle}>
                   <img src="/images/google_login.png" />
                 </button>
               </div>
-            </form>
-
-            <button onClick={signOut}>임시로그아웃</button>
-
-            <div>
-              <p onClick={() => setMode(!mode)}>회원가입 하기</p>
             </div>
-          </div>
+          </form>
+
+          <p
+            onClick={() => setMode(!mode)}
+            className="mt-10 text-center text-sm text-blue-700 hover:text-blue-500 cursor-pointer"
+          >
+            회원가입 하기
+          </p>
+          <button onClick={signOut}>임시로그아웃</button>
         </div>
       </div>
     </>
