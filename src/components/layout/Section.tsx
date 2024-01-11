da7f9139ba02a54477f41da65986a3b42d78b2bc
dosion9 <dosion9ood@gmail.com>
2024-01-11 23:23:40 +0900

    update : section h1 style 수정

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 interface Props {
   title: string | null;
+  isCenter?: boolean;
+  className?: string;
   children: React.ReactNode;
 }
 
-const Section = ({ title, children }: Props) => {
+const Section = ({ title, isCenter = false, className, children }: Props) => {
   return (
     <section className="mb-[200px]">
-      {title && <h1 className="font-bold text-[30px] mb-[60px]">{title}</h1>}
+      {title && (
+        <h1 className={`${isCenter && "text-center"} font-bold  text-[30px] mb-[60px] ${className}`}>{title}</h1>
+      )}
       <div>{children}</div>
     </section>
   );
