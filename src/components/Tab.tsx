a8b3e148f65444785a533d71badd59c89d4e7b4c
silo9506 <silo9506@gmial.com>
2024-01-12 16:04:54 +0900

    update:isVariable여부를 통한 2가지 스타일 Tab을 반환하도록 수정

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -5,25 +5,39 @@ interface TabProps {
   activeTab: string;
   handleTabClick: Function;
   tabs: string[];
+  isVariable?: boolean;
 }
 
-const Tab = ({ activeTab, handleTabClick, tabs }: TabProps) => {
+const Tab = ({ activeTab, handleTabClick, tabs, isVariable = false }: TabProps) => {
   return (
-    <ul className="flex w-full items-center justify-center gap-[20px]">
-      {tabs.map((tab, index) => (
-        <li
-          onClick={() => handleTabClick(tab)}
-          className={` px-[20px] py-[10px] text-center rounded-3xl cursor-pointer  border-neutral-400 border    ${
-            activeTab === tab ? "bg-[#3074F0]  text-white" : "bg-white text-black"
-          }`}
-          // className={`flex-1 text-center cursor-pointer  border-neutral-400 border-4 rounded-sm p-1  ${
-          //   activeTab === index ? "bg-neutral-400  text-black" : "bg-white"
-          // }`}
-          key={tab}
-        >
-          {tab}
-        </li>
-      ))}
+    <ul className={`flex w-full items-center justify-center ${isVariable && "gap-[20px]"}`}>
+      {tabs.map((tab, index) => {
+        if (isVariable) {
+          return (
+            <li
+              onClick={() => handleTabClick(tab)}
+              className={` px-[20px] py-[10px] text-center rounded-3xl cursor-pointer  border-neutral-400 border    ${
+                activeTab === tab ? "bg-[#3074F0]  text-white" : "bg-white text-black"
+              }`}
+              key={tab}
+            >
+              {tab}
+            </li>
+          );
+        } else {
+          return (
+            <li
+              className={`flex-1 text-center cursor-pointer  border-neutral-400 border-4 rounded-sm p-1  ${
+                activeTab === tab ? "bg-neutral-400  text-black" : "bg-white"
+              }`}
+              onClick={() => handleTabClick(tab)}
+            >
+              {" "}
+              {tab}
+            </li>
+          );
+        }
+      })}
     </ul>
   );
 };
