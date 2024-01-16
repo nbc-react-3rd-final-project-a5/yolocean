790d77a46bdd83e5ff414a633e4c0e7e1db875b0
yesol <cloud.mys.99@gmail.com>
2024-01-16 21:25:10 +0900

    feat: 로그인 유저 카트 불러오기 (#27)

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,11 +5,11 @@ import CartItem from "./CartItem";
 import { useCart } from "@/hooks";
 import Link from "next/link";
 import PageBreadCrumb from "@/components/layout/PageBreadCrumb";
-import { useAuth } from "@/hooks";
+
 import { useQuery } from "@tanstack/react-query";
 import useLogedInStore from "@/store/logedStore";
 import { useRouter } from "next/navigation";
-import { MdNumbers } from "react-icons/md";
+import { useAuthStore } from "@/store/authStore";
 
 const linkList = [
   {
@@ -51,8 +51,9 @@ const page = () => {
   //   alert("로그인 후 이용해주세요");
   //   router.push("/");
   // }
+  const { auth } = useAuthStore();
 
-  const { cart, isLoading } = useCart({ userId: "aba26c49-82c0-42b2-913c-c7676527b553", cartId: "" });
+  const { cart, isLoading } = useCart({ userId: auth, cartId: "" });
 
   const [cartPrice, setCartPrice] = useState<number[]>([]);
 
@@ -61,42 +62,46 @@ const page = () => {
       <PageBreadCrumb linkList={linkList} />
       <Section title={"장바구니"} isCenter={true}>
         {!isLoading ? (
-          <div>
-            <div className="flex flex-col items-center justify-center">
-              {(cart as CartBox[]).map((cartItem, idx) => {
-                return (
-                  cartItem && (
-                    <CartItem
-                      cart={cartItem}
-                      cartPrice={cartPrice}
-                      setCartPrice={setCartPrice}
-                      idx={idx}
-                      key={cartItem.id}
-                    />
-                  )
-                );
-              })}
-            </div>
-
-            <div className="mt-[80px] flex flex-col content-end">
-              <div className="mb-[30px]">
-                <div className="text-right text-[16px] font-bold mr-1">
-                  총 결제금액
-                  <p className="text-[24px] font-bold inline-block ml-1">
-                    {" "}
-                    {cartPrice.reduce((acc, num) => acc + num, 0)}원
-                  </p>
-                </div>
+          cart !== undefined ? (
+            <div>
+              <div className="flex flex-col items-center justify-center">
+                {(cart as CartBox[]).map((cartItem, idx) => {
+                  return (
+                    cartItem && (
+                      <CartItem
+                        cart={cartItem}
+                        cartPrice={cartPrice}
+                        setCartPrice={setCartPrice}
+                        idx={idx}
+                        key={cartItem.id}
+                      />
+                    )
+                  );
+                })}
               </div>
 
-              <button
-                onClick={() => router.push("/payment")}
-                className="w-[244px] h-[50px] bg-point text-white rounded-[5px] ml-[80%]"
-              >
-                결제하기
-              </button>
+              <div className="mt-[80px] flex flex-col content-end">
+                <div className="mb-[30px]">
+                  <div className="text-right text-[16px] font-bold mr-1">
+                    총 결제금액
+                    <p className="text-[24px] font-bold inline-block ml-1">
+                      {" "}
+                      {cartPrice.reduce((acc, num) => acc + num, 0)}원
+                    </p>
+                  </div>
+                </div>
+
+                <button
+                  onClick={() => router.push("/payment")}
+                  className="w-[244px] h-[50px] bg-point text-white rounded-[5px] ml-[80%]"
+                >
+                  결제하기
+                </button>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div>카트 불러오는 중...</div>
+          )
         ) : (
           <div>Loading...</div>
         )}
