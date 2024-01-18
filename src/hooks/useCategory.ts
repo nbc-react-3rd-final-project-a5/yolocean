705a00f23495fe26387f825d580759ceaeff7704
Majinwoo <maddonge@naver.com>
2024-01-18 11:40:40 +0900

    refact: React Query를 사용해 category 불러오는 부분 수정

--- src/hooks/useCategory.ts
+++ src/hooks/useCategory.ts
@@ -1,38 +1,22 @@
 import { CategoryTable } from "@/types/db";
 import { useQuery } from "@tanstack/react-query";
 
-const useCategory = () => {
+const useCategory = (url?: string) => {
   const {
     data: category,
     isLoading,
     isError
   } = useQuery<CategoryTable[]>({
     queryFn: async (): Promise<CategoryTable[]> => {
-      const response = await fetch(`/api/category`, { method: "GET" });
+      const response = await fetch(`/api/category${url ? `/${url}` : ""}`, { method: "GET" });
       const data = await response.json();
       return data;
     },
 
-    queryKey: ["category"]
+    queryKey: url ? ["category", url] : ["category"]
   });
 
   return { category, isLoading };
 };
 
-const getCategoryNameById = (categoryId: string) => {
-  const {
-    data: categoryName,
-    isLoading,
-    isError
-  } = useQuery<string>({
-    queryFn: async (): Promise<string> => {
-      const response = await fetch(`/api/category/${categoryId}`, { method: "GET" });
-      const data = await response.json();
-      return data;
-    },
-    queryKey: ["category", categoryId]
-  });
-
-  return { categoryName, isLoading };
-};
-export { useCategory, getCategoryNameById };
+export default useCategory;
