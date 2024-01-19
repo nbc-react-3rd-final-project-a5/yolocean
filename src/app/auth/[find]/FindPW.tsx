02e9afb3d02331cf77bc13c81c67b6742415d0fd
yesol <cloud.mys.99@gmail.com>
2024-01-19 12:15:36 +0900

    feat: 비밀번호 재설정 (#85)

--- src/app/auth/[find]/FindPW.tsx
+++ src/app/auth/[find]/FindPW.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import PageBreadCrumb from "@/components/layout/PageBreadCrumb";
 import { useForm, SubmitHandler } from "react-hook-form";
-
+import { supabase } from "@/service/supabase";
+import ResultPW from "./ResultPW";
+import { useRouter } from "next/router";
 interface Props {
   setMode: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 interface FormValue {
-  id: string;
+  email: string;
   name: string;
   phone: string;
 }
@@ -31,7 +33,7 @@ const FindPW = ({ setMode }: Props) => {
   //찾기
   const [find, setFind] = useState(false);
   //찾기 결과
-  const [result, setResult] = useState<boolean | string>(false);
+  const [result, setResult] = useState<string>("");
 
   const {
     register,
@@ -39,14 +41,29 @@ const FindPW = ({ setMode }: Props) => {
     formState: { errors }
   } = useForm<FormValue>({ mode: "onBlur" });
 
-  const onSubmit: SubmitHandler<FormValue> = (inputData) => {
+  const onSubmit: SubmitHandler<FormValue> = async (inputData) => {
     //비밀번호 찾기 버튼 눌렀을 때
-    console.log(inputData);
+    setFind(true);
+    const { data, error } = await supabase
+      .from("userinfo")
+      .select("id")
+      .match({ email: inputData.email, phone: inputData.phone });
+    if (error) {
+      window.confirm("supabase error");
+    } else {
+      setResult(data[0]?.id || "");
+    }
   };
   return (
     <>
       <PageBreadCrumb linkList={linkList} />
-      <div className="flex justify-center items-center space-x-[40px] text-center text-[25px] font-semibold mb-[80px] ">
+      <div
+        className={
+          result === ""
+            ? "flex justify-center items-center space-x-[40px] text-center text-[25px] font-semibold mb-[80px] "
+            : "hidden"
+        }
+      >
         <p onClick={() => setMode(true)} className="text-tc-light cursor-pointer">
           아이디 찾기
         </p>
@@ -55,65 +72,69 @@ const FindPW = ({ setMode }: Props) => {
 
       <div className="flex flex-col justify-center items-center ">
         <div className="w-[345px]">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-[15px]">
-              <label htmlFor="email" className="">
-                Email address
-                <span className="text-xs text-red-600">{errors?.id?.message}</span>
-              </label>
+          {!find ? (
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-[15px]">
+                <label htmlFor="email" className="block mb-2">
+                  Email address
+                  <span className="inline-block ml-2 text-[12px] text-red-600">{errors?.email?.message}</span>
+                </label>
 
-              <input
-                id="email"
-                type="email"
-                {...register("id", {
-                  required: "   이메일을 입력하세요.",
-                  pattern: {
-                    value: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/,
-                    message: "   이메일 형식이 유효하지 않습니다."
-                  }
-                })}
-                placeholder="이메일"
-                className="block w-full h-[50px] border p-[15px]"
-              />
-            </div>
-            <div className="mb-[15px]">
-              <label htmlFor="email" className="">
-                Username
-                <span className="text-xs text-red-600">{errors?.name?.message}</span>
-              </label>
-              <input
-                id="name"
-                type="text"
-                {...register("name", {
-                  required: "   이름을 입력하세요."
-                })}
-                placeholder="이름"
-                className="block w-full h-[50px] border p-[15px]"
-              />
-            </div>
-
-            <div className="mb-[20px]">
-              <label htmlFor="phone" className="">
-                Phone Number
-                <span className="text-xs text-red-600">{errors?.phone?.message}</span>
-              </label>
-              <div className="grid grid-cols-5 w-full  border">
                 <input
-                  id="phone"
-                  type="phone"
-                  placeholder="휴대폰번호"
-                  {...register("phone", {
-                    required: "   휴대폰번호를 입력하세요"
+                  id="email"
+                  type="email"
+                  {...register("email", {
+                    required: "이메일을 입력하세요.",
+                    pattern: {
+                      value: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/,
+                      message: "이메일 형식이 유효하지 않습니다."
+                    }
                   })}
-                  className="block col-span-4 h-[50px] p-[15px]"
+                  placeholder="이메일"
+                  className="block w-full h-[50px] border p-[15px]"
                 />
-                <button className="border-l h-[50px] text-tc-middle font-normal">인증하기</button>
               </div>
-            </div>
-            <button type="submit" className="h-[50px] rounded-[5px] w-[100%] bg-point text-white">
-              비밀번호 찾기
-            </button>
-          </form>
+              <div className="mb-[15px]">
+                <label htmlFor="email" className="block mb-2">
+                  Username
+                  <span className="inline-block ml-2 text-[12px] text-red-600">{errors?.name?.message}</span>
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  {...register("name", {
+                    required: "이름을 입력하세요."
+                  })}
+                  placeholder="이름"
+                  className="block w-full h-[50px] border p-[15px]"
+                />
+              </div>
+
+              <div className="mb-[20px]">
+                <label htmlFor="phone" className="block mb-2">
+                  Phone Number
+                  <span className="inline-block ml-2 text-[12px] text-red-600">{errors?.phone?.message}</span>
+                </label>
+                <div className="grid grid-cols-5 w-full  border">
+                  <input
+                    id="phone"
+                    type="phone"
+                    placeholder="휴대폰번호"
+                    {...register("phone", {
+                      required: "휴대폰번호를 입력하세요"
+                    })}
+                    className="block col-span-4 h-[50px] p-[15px]"
+                  />
+                  <button className="border-l h-[50px] text-tc-middle font-normal">인증하기</button>
+                </div>
+              </div>
+              <button type="submit" className="h-[50px] rounded-[5px] w-[100%] bg-point text-white">
+                비밀번호 찾기
+              </button>
+            </form>
+          ) : (
+            <ResultPW result={result} setFind={setFind} setMode={setMode} />
+          )}
         </div>
       </div>
     </>
