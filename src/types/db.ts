5a2d80b2502202dfaa670753ab6677da3334ec6e
dosion9 <dosion9ood@gmail.com>
2024-01-22 11:33:47 +0900

    Add files via upload

---
+++ src/types/db.ts
@@ -0,0 +1,56 @@
+import { Tables, TablesInsert } from "./supabase";
+
+export type Cart = Tables<"cart">;
+export type CategoryTable = Tables<"category">;
+export type Product = Tables<"product">;
+export type Qna = Tables<"qna">;
+export type Rent = Tables<"rent">;
+export type Review = Tables<"review">;
+export type Stock = Tables<"stock">;
+export type Store = Tables<"store">;
+export type UserInfo = Tables<"userinfo">;
+export type Region = Tables<"region">;
+export type RentInsert = TablesInsert<"rent">;
+export interface ProductProperties extends Product {
+  info: [string];
+  category: {
+    category_name: string;
+  };
+  stock: {
+    count: number;
+    store: {
+      name: string;
+      address: string;
+    }[];
+  };
+}
+export interface StoreWithStock extends Store {
+  stock: [{ count: number }];
+}
+export interface ExtendReview extends Review {
+  store: { name: string; region: { region: string } };
+  userinfo: { username: string; avatar_url: string };
+  product: { name: string; thumbnail: string; category_id: string };
+  url: string[] | null;
+}
+
+export interface CartBox {
+  count: number | null;
+  id: string;
+  product_id: string | null;
+  store_id: string | null;
+  user_id: string;
+  rent_date: string;
+  product: {
+    name: string;
+    thumbnail: string;
+    price: number;
+    percentage_off: number;
+    category: {
+      category_name: string;
+    };
+  };
+  store: {
+    name: string;
+  };
+}
