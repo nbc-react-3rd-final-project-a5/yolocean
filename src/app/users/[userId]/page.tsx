46d384b6bdeb321e49733df10e57223e4c52023c
dosion9 <dosion9ood@gmail.com>
2024-01-22 11:17:50 +0900

    feat : 마이페이지 기능

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,22 +1,58 @@
 import React from "react";
 import UserInfoSection from "./UserInfoSection";
 import PageBreadCrumb from "@/components/layout/PageBreadCrumb";
+import UserTab from "./UserTab";
+import UserReviewList from "./(tabContent)/UserReviewList";
+import UserQnaList from "./(tabContent)/UserQnaList";
+import UserReservationList from "./(tabContent)/UserReservationList";
+import UserRentList from "./(tabContent)/UserRentList";
+
+interface Props {
+  params: { userId: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+enum EnumTabList {
+  reservation = "예약내역",
+  rent = "렌트완료",
+  review = "작성한 리뷰",
+  qna = "Q&A"
+}
+
 const linkList = [
   {
     name: "홈",
-    url: "http://localhost:3000/"
+    url: "https://yolocean.vercel.app/"
   },
   {
     name: "마이페이지",
-    url: "http://localhost:3000/"
+    url: "https://yolocean.vercel.app/"
   }
 ];
-const MyPage = () => {
+
+const MyPage = ({ params, searchParams }: Props) => {
+  const { userId } = params;
+  const activeTab = searchParams?.activeTab;
+
+  const currentTap = (activeTab: string | string[] | undefined) => {
+    switch (activeTab) {
+      case "rent":
+        return <UserRentList />;
+      case "review":
+        return <UserReviewList userId={userId} />;
+      case "qna":
+        return <UserQnaList userId={userId} />;
+      default:
+        return <UserReservationList userId={userId} />;
+    }
+  };
   return (
-    <div>
+    <>
       <PageBreadCrumb linkList={linkList} />
       <UserInfoSection />
-    </div>
+      <UserTab className="mt-[78px] mb-[40px]" />
+      {currentTap(activeTab)}
+    </>
   );
 };
 
