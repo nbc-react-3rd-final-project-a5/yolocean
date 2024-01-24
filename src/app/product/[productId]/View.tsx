c3e311adf2aaa7fa4738ec73d90f3864dffb9e65
peeChulchul <eyeless356@naver.com>
2024-01-24 22:41:30 +0900

    feat:users페이지 및 api라우트의 userId유효성체크 구현

--- src/app/product/[productId]/View.tsx
+++ src/app/product/[productId]/View.tsx
@@ -6,6 +6,7 @@ interface Props {
 }
 
 const View = ({ product }: Props) => {
+  console.log(product.view);
   useEffect(() => {
     const visitProduct = sessionStorage.getItem(product.id);
     if (visitProduct) return;
@@ -19,3 +20,10 @@ const View = ({ product }: Props) => {
 };
 
 export default View;
+
+// 조회수를 구현하려고하는데 상품페이지 자체는 서버클라이언트입니다.
+// 상품페이지에 들어오면 view컴포넌트(클라이언트)에서 props로 상품정보를 받아
+// 세션스토리지에 상품 id가 저장되고 조회수를 기존조회수 +1을 해줍니다.
+// 만약 아이디가 있다면 아무일도 일어나지않습니다.
+
+// 문제는 서버컴포넌트에서 받아온 상품정보의 view가 1이라는점입니다.
