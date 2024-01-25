702e871b47dd574313fe0cc5e6433baf4757146d
yesol <cloud.mys.99@gmail.com>
2024-01-25 12:28:11 +0900

    style: dropdown 스타일링

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,26 +4,21 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import type { Database } from "./types/supabase";
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-
+const getUserId = async (req: NextRequest, res: NextResponse) => {
   // Create a Supabase client configured to use cookies
   const supabase = createMiddlewareClient<Database>({ req, res });
-
-  // Refresh session if expired - required for Server Components
-  //   const { data, error } = await supabase.auth.getSession();
-  //   console.log("in middleware", data || error);
-
   let authCookie = req.cookies.get("sb-hntpomvsqgbdpwrjnsun-auth-token");
   if (authCookie !== undefined) {
     const tokens = JSON.parse(authCookie.value);
-    const {
-      data: { user }
-    } = await supabase.auth.getUser(tokens[0]);
-    console.log("누구야: ", user);
+    const { data } = await supabase.auth.getUser(tokens[0]);
+    if (!data.user) return false;
+    const { user } = data;
+    return user.id;
   }
-  return res;
-}
+  return false;
+};
+
+export async function middleware(req: NextRequest) {}
 
 // Ensure the middleware is only called for relevant paths.
 export const config = {
@@ -34,6 +29,9 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!_next/static|_next/image|favicon.ico).*)"
+    // "/((?!_next/static|_next/image|favicon.ico).*)",
+    // "/cart/:path*"
+    "/api/user/:path*",
+    "/users/:path*"
   ]
 };
