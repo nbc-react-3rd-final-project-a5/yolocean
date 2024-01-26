d47067608f874761ef538ee46f2c8a439047bc0a
dosion9 <dosion9ood@gmail.com>
2024-01-26 11:24:45 +0900

    feat : 커스텀 링크

---
+++ src/components/CustomLink.tsx
@@ -0,0 +1,81 @@
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  href: string;
+  children: React.ReactNode;
+  onClick?: React.MouseEventHandler;
+  disabled?: boolean;
+  size?: keyof typeof EnumSize;
+  fontWeight?: keyof typeof EnumFontWeight;
+  color?: keyof typeof EnumColor;
+  isOutline?: boolean;
+  isFull?: boolean;
+}
+
+enum EnumSize {
+  sm = "px-[2em] py-[0.5em] min-w-[5rem] text-[14px]",
+  md = "px-[2em] py-[0.5em] min-w-[10rem] text-[16px]",
+  lg = "px-[2em] py-[0.5em] min-w-[15rem] text-[18px]"
+}
+
+enum EnumFontWeight {
+  thin = "font-light",
+  default = "font-normal",
+  bold = "font-semibold"
+}
+
+enum EnumBg {
+  blue = "bg-point",
+  gray = "bg-tc-middle",
+  white = "bg-white"
+}
+
+enum EnumColor {
+  blue = "text-point",
+  gray = "text-tc-white",
+  white = "text-white"
+}
+
+// color 값으로 색상 가져오기
+enum EnumBorder {
+  blue = "border-point",
+  gray = "border-tc-middle",
+  white = "border-tc-middle"
+}
+
+const CustomLink = ({
+  href,
+  children,
+  onClick,
+  disabled = false,
+  size = "md",
+  fontWeight = "default",
+  color = "blue",
+  isOutline = false,
+  isFull = false
+}: Props) => {
+  const disabledStyle = " bg-line text-tc-light";
+  const defaultStyle = "inline-block text-center border rounded-[5px]";
+  const customStyleObj = {
+    size: EnumSize[size],
+    fontWeight: EnumFontWeight[fontWeight],
+    bg: isOutline ? EnumBg["white"] : EnumBg[color],
+    color: isOutline ? EnumColor[color] : color === "white" ? EnumColor["gray"] : EnumColor["white"],
+    border: disabled ? "border-line" : EnumBorder[color],
+    curser: disabled && "cursor-default"
+  };
+  const customStyle = Object.values(customStyleObj).join(" ");
+
+  return (
+    <Link
+      href={disabled ? "#" : href}
+      onClick={onClick}
+      className={`${defaultStyle} ${customStyle}  ${isFull && "w-full"} ${disabled && disabledStyle} `}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default CustomLink;
