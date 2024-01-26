b34e8010666aeb1006fb69d4bb6484bec377f979
peeChulchul <eyeless356@naver.com>
2024-01-26 17:10:32 +0900

    카드리스트 gap 수정

--- src/components/CardLists.tsx
+++ src/components/CardLists.tsx
@@ -5,7 +5,7 @@ import { ProductProperties } from "@/types/db";
 const CardLists = ({ cardLists }: { cardLists: ProductProperties[] }) => {
   return (
     <div>
-      <div className="grid grid-cols-4 gap-[30px]   mobile:grid-cols-2 tablet:grid-cols-3">
+      <div className="grid grid-cols-4 gap-[30px]  mobile:gap-[10px] tablet:gap-[15px]  mobile:grid-cols-2 tablet:grid-cols-3">
         {cardLists.map((item: ProductProperties) => (
           <Card categoryId={item.category_id!} product={item} key={item.id} />
         ))}
