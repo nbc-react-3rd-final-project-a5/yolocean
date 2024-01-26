a5ac2cf8b6f40c4165437b9488471b10957aaad1
yesol <cloud.mys.99@gmail.com>
2024-01-26 12:26:43 +0900

    style: 카트 아이템 스켈레톤 적용(#124)

--- src/components/pulse/CartPulse.tsx
+++ src/components/pulse/CartPulse.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import ImgPulse from "./ImgPulse";
+import { FaRegImage } from "react-icons/fa6";
 
 const CartPulse = () => {
   return (
-    <div role="status" className="animate-pulse space-y-8 w-[100%] flex flex-col my-2 border-y">
-      <div className="flex flex-row mt-5">
-        <div className="mx-5 w-[190px] h-[190px] relative">
-          <ImgPulse />
+    <div
+      role="status"
+      className="animate-pulse space-y-8 w-[100%] flex flex-col my-2 border-y mobile:place-items-center"
+    >
+      <div className="flex flex-row my-5 mobile:place-items-center">
+        <div className="flex justify-center items-center mx-5 w-[190px] h-[190px] bg-gray-200 mobile:mx-1 mobile:w-[100px] mobile:h-[100px]">
+          <FaRegImage className="text-tc-light" />
         </div>
-        <div className="mx-3 ">
-          <div className="h-4 bg-gray-200 rounded-full w-48 mb-4"></div>
+        <div className="px-3 py-5">
+          <div className="h-4 bg-gray-200 rounded-full w-48 mb-4 mobile:w-25"></div>
           <div className="h-3 bg-gray-200 rounded-full max-w-[480px] mb-2.5"></div>
           <div className="h-3 bg-gray-200 rounded-full mb-2.5"></div>
           <div className="h-3 bg-gray-200 rounded-full max-w-[440px] mb-2.5"></div>
           <div className="h-3 bg-gray-200 rounded-full max-w-[460px] mb-2.5"></div>
           <div className="h-3 bg-gray-200 rounded-full max-w-[360px]"></div>
         </div>
-      </div>
-      <div className=" flex flex-row justify-between mt-[20px]">
-        <div className="h-3 bg-gray-200 rounded-full w-48"></div>
-        <div className="h-4 bg-gray-200 rounded-full w-48 mb-4"></div>
       </div>
     </div>
   );
