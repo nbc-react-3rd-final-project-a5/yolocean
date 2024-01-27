4dcdd7542f3c59bb5f4b7736c67641dd05e5bb2e
Majinwoo <maddonge@naver.com>
2024-01-27 22:09:23 +0900

    fix: 메인페이지 리뷰 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,16 +64,16 @@ const Home = async () => {
       {main01Banner && <Banner banner={main02Banner!} />}
 
       <Section title="재밌게 즐기구 돌아왔션 ✌️" isCenter={false}>
-        <div className="grid grid-cols-4 gap-[30px]  mobile:gap-[10px] tablet:gap-[15px]  mobile:grid-cols-2 tablet:grid-cols-3">
+        <div className="grid grid-cols-4 gap-[20px]  mobile:gap-[10px] tablet:gap-[15px]  mobile:grid-cols-2 tablet:grid-cols-3">
           {reviews.map((fixedReview: ExtendFixedReview) => (
-            <Link key={fixedReview.id} href={`/product/${fixedReview.review.product_id}#후기`}>
-              <div>
-                <img
-                  className="mobile:max-w-[160px] mobile:h-[160px] tablet:max-w-[180px] tablet:h-[180px] max-w-[246px] w-full h-[246px] bg-bg "
-                  src={fixedReview.review.url[0]}
-                />
-              </div>
-            </Link>
+            <div
+              key={fixedReview.id}
+              className="mobile:max-w-[160px] mobile:h-[160px] tablet:max-w-[180px] tablet:h-[180px] max-w-[246px] w-full h-[246px] bg-bg "
+            >
+              <Link href={`/product/${fixedReview.review.product_id}#후기`}>
+                <img className="w-full h-full" src={fixedReview.review.url[0]} />
+              </Link>
+            </div>
           ))}
         </div>
       </Section>
