9fa0e860c2318e01703d9ff1754fa44af4542553
Majinwoo <maddonge@naver.com>
2024-01-27 16:49:19 +0900

    fix: 카테고리페이지 도메인변경

--- src/app/category/[categoryId]/page.tsx
+++ src/app/category/[categoryId]/page.tsx
@@ -4,7 +4,6 @@ import CategoryHeader from "./CategoryHeader";
 import getPath from "@/utils/getPath";
 
 const getCategoryName = async ({ categoryId }: { categoryId: string }): Promise<string> => {
-  const { domain } = getPath();
   const result = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/category/${categoryId}`, { method: "GET" });
   if (!result.ok) {
     throw new Error("카테고리 데이터 불러오기 실패");
