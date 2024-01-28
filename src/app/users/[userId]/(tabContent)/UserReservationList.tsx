947378bff240dff4204d97847399c5dc0029372a
dosion9 <dosion9ood@gmail.com>
2024-01-29 08:54:25 +0900

    refact : 예약 내역 SSR에서 query 사용으로 변경

--- src/app/users/[userId]/(tabContent)/UserReservationList.tsx
+++ src/app/users/[userId]/(tabContent)/UserReservationList.tsx
@@ -1,18 +1,36 @@
+"use client";
+
 import React from "react";
 import RentItem from "./RentItem";
 import { getAllUserRent } from "@/service/table";
+import { useQuery } from "@tanstack/react-query";
+import UserRentPulse from "@/components/pulse/UserRentPulse";
 
 interface Props {
   userId: string;
 }
 
-const UserReservationList = async ({ userId }: Props) => {
-  const reservationList = await getAllUserRent({ userId });
+// 예약내역 탭
+const UserReservationList = ({ userId }: Props) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["reservation", userId],
+    queryFn: async () => getAllUserRent({ userId })
+  });
+
+  if (isLoading)
+    return (
+      <ul>
+        <li className="first:border-t border-t border-b border-line py-5">
+          <UserRentPulse />
+        </li>
+      </ul>
+    );
+
   return (
-    <div>
-      {reservationList?.length > 0 ? (
+    <>
+      {data?.length > 0 ? (
         <ul>
-          {reservationList.map((n: any) => {
+          {data.map((n: any) => {
             return (
               <li key={`rentItem`} className="first:border-t border-t border-b border-line py-5">
                 <RentItem rentData={n} isReturn={false} />
@@ -23,7 +41,7 @@ const UserReservationList = async ({ userId }: Props) => {
       ) : (
         <div className="w-full text-center text-[18px] font-semibold"> 예약 내역이 없습니다 😅</div>
       )}
-    </div>
+    </>
   );
 };
 
