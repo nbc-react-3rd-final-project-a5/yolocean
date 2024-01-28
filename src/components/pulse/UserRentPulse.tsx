b67686e2289101bc89215534299e2584b0d82b5a
dosion9 <dosion9ood@gmail.com>
2024-01-29 08:53:14 +0900

    feat : mypage의 rent pulse 생성

---
+++ src/components/pulse/UserRentPulse.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ImgPulse from "./ImgPulse";
+
+const UserRentPulse = () => {
+  return (
+    <div className="flex animate-pulse">
+      <figure className="relative aspect-square w-[20%] mr-[5%] h-[190px] flex items-center">
+        <ImgPulse />
+      </figure>
+
+      <div className=" w-[75%] ">
+        <div className="mb-[20px] bg-gray-200 rounded-full w-[8rem] h-[1rem]"></div>
+        <div className="mb-[10px] bg-gray-200 rounded-full w-[6rem] h-[1rem]"></div>
+        <div className="mb-[15px] bg-gray-200 rounded-full w-[15rem] h-[1rem]"></div>
+        <div className="mb-[15px] bg-gray-200 rounded-full w-[15rem] h-[1rem]"></div>
+        <div className="mb-[15px] bg-gray-200 rounded-full w-[5rem] h-[1rem]"></div>
+        <div className="mb-[15px] bg-gray-200 rounded-full w-[3rem] h-[1rem]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default UserRentPulse;
