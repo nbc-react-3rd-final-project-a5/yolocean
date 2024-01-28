847f938774908af26712b993d4d7396b3645b4a0
dosion9 <dosion9ood@gmail.com>
2024-01-29 08:54:39 +0900

    refact : 렌트 완료 SSR에서 query 사용으로 변경

--- src/app/users/[userId]/(tabContent)/UserRentList.tsx
+++ src/app/users/[userId]/(tabContent)/UserRentList.tsx
@@ -1,19 +1,38 @@
+"use client";
+
 import { getAllUserRent } from "@/service/table";
 import React from "react";
 import RentItem from "./RentItem";
+import { useQuery } from "@tanstack/react-query";
+import UserRentPulse from "@/components/pulse/UserRentPulse";
 
 interface Props {
   userId: string;
 }
-
+// 렌트 완료 탭
 // TODO : 테이블 리턴 값이 true 인 상품을 가져올 수 있도록 API 설정할 것
-const UserRentList = async ({ userId }: Props) => {
-  const reservationList = await getAllUserRent({ userId });
-  return (
-    <div>
-      {reservationList?.length > 0 ? (
+const UserRentList = ({ userId }: Props) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["rent", userId],
+    queryFn: async () => getAllUserRent({ userId })
+  });
+
+  if (isLoading)
+    return (
+      <>
         <ul>
-          {reservationList.map((n: any) => {
+          <li className="first:border-t border-t border-b border-line py-5">
+            <UserRentPulse />
+          </li>
+        </ul>
+      </>
+    );
+
+  return (
+    <>
+      {data?.length > 0 ? (
+        <ul>
+          {data.map((n: any) => {
             return (
               <li key={`rentItem`} className="first:border-t border-t border-b border-line py-5">
                 <RentItem rentData={n} isReturn={true} />
@@ -24,7 +43,7 @@ const UserRentList = async ({ userId }: Props) => {
       ) : (
         <div className="w-full text-center text-[18px] font-semibold"> 렌트 내역이 없습니다 😅</div>
       )}
-    </div>
+    </>
   );
 };
 
