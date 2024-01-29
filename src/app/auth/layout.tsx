3761fe881bf65e6e5bbe2b907e25060487863999
Majinwoo <maddonge@naver.com>
2024-01-29 16:36:43 +0900

    feat: cart, auth, category 페이지 metadata 생성

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "YOLOCEAN - 로그인/회원가입",
+  description: "욜로오션의 회원이 되어 보세요! 🌊🌊🌊",
+  openGraph: {
+    images: ["/opengraph-image.png"]
+  }
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const layout = ({ children }: Props) => {
+  return <>{children}</>;
+};
+
+export default layout;
