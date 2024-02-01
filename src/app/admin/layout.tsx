64309921ff44834aca64dfbe7b714a8294d371c5
Majinwoo <maddonge@naver.com>
2024-02-01 17:41:56 +0900

    style: admin 페이지 layout 스타일 추가 (#178)

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,7 +5,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex max-w-[1200px] w-full border-2 border-point rounded-lg">
       <NavigationBar />
-      {children}
+      <div className="mx-auto flex flex-col items-center gap-1">{children}</div>
     </div>
   );
 };
