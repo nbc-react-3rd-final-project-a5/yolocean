68f7a4381a4039ba751e76ce4d2a52d673ae47c2
dosion9 <dosion9ood@gmail.com>
2024-02-02 22:18:32 +0900

    feat : banner 생성 완료

--- src/app/admin/promotion/(Banner)/BannerForm.tsx
+++ src/app/admin/promotion/(Banner)/BannerForm.tsx
@@ -2,17 +2,27 @@ import CustomButton from "@/components/CustomButton";
 import InputImage from "@/components/InputImage";
 import { useCustomMutation, useImageInput } from "@/hook";
 import { createBanner, deleteBanner, updateBanner } from "@/service/table/banner";
+import { usealertStore } from "@/store/alertStore";
 import { Banner } from "@/types/db";
-import React, { useEffect } from "react";
+import useStorage from "@/utils/useStorage";
+import Link from "next/link";
+import React, { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
 
 interface Props {
   selectBanner?: Banner;
+  isCreateMode: boolean;
 }
 
-const BannerForm = ({ selectBanner }: Props) => {
+const BannerForm = ({ selectBanner, isCreateMode }: Props) => {
+  const { alertFire } = usealertStore();
+  const { uploadImage } = useStorage();
   const { customImageList, isEnter, handler, addPreImage } = useImageInput(1);
-  const { mutate: createMutation } = useCustomMutation({ queryKey: ["ManageBanner"], mutationFn: createBanner });
+  const { mutate: createMutation } = useCustomMutation({
+    queryKey: ["ManageBanner"],
+    mutationFn: (data) => createBanner(data)
+  });
   const { mutate: updateMutation } = useCustomMutation({
     queryKey: ["ManageBanner"],
     mutationFn: (data) => updateBanner(selectBanner!.banner_name, data)
@@ -39,44 +49,82 @@ const BannerForm = ({ selectBanner }: Props) => {
     }
   }, [selectBanner]);
 
+  useEffect(() => {
+    if (isCreateMode) {
+      setValue("id", "");
+      setValue("banner_link", "");
+      setValue("banner_name", "");
+    }
+  }, [isCreateMode]);
+
+  useEffect(() => {
+    if (customImageList[0]) {
+      setValue("id", customImageList[0].id);
+    } else {
+      setValue("id", "");
+    }
+  }, [customImageList[0]]);
+
+  const handleCreateSubmit = async (data: Omit<Banner, "url">) => {
+    const storagePath = `promotion/banner`;
+
+    try {
+      if (customImageList[0].file) {
+        data.id = customImageList[0].id;
+        data.banner_url = await uploadImage(customImageList[0].file, "promotion", data.id, storagePath);
+      }
+
+      createMutation(data);
+      return alertFire("배너 업로드 성공", "success");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      {selectBanner ? (
-        <form className="flex flex-col gap-4">
-          <div>
-            <label htmlFor="banner-id" className="block mb-2 font-semibold">
-              배너 ID
-            </label>
-            <input id="banner-id" disabled {...register("id")} className="w-full p-2" />
-          </div>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(handleCreateSubmit)}>
+        <div>
+          <label htmlFor="banner-id" className="block mb-2 font-semibold">
+            배너 ID
+          </label>
+          <input id="banner-id" disabled {...register("id")} className="w-full p-2" />
+        </div>
 
+        <div>
+          <label htmlFor="banner_name" className="block mb-2 font-semibold">
+            배너 위치
+          </label>
+          <input id="banner_name" {...register("banner_name")} className="w-full  p-2" />
+        </div>
+        <div>
+          <label htmlFor="banner_link" className="block mb-2 font-semibold">
+            이동 링크
+          </label>
+          <input id="banner_link" {...register("banner_link")} className="w-full  p-2" />
+        </div>
+        {isCreateMode && selectBanner && (
           <div>
-            <label htmlFor="banner_name" className="block mb-2 font-semibold">
-              배너 위치
-            </label>
-            <input id="banner_name" {...register("banner_name")} className="w-full  p-2" />
+            <p className=" mb-2 font-semibold">이미지 주소</p>
+            <Link href={selectBanner.banner_url} target="_blank" className="w-full  p-2 text-[0.8rem] text-point">
+              {selectBanner.banner_url}
+            </Link>
           </div>
-          <div>
-            <label htmlFor="banner_link" className="block mb-2 font-semibold">
-              이동 링크
-            </label>
-            <input id="banner_link" {...register("banner_link")} className="w-full  p-2" />
-          </div>
-          <div>
-            <label htmlFor="banner_image" className="block mb-2 font-semibold">
-              이미지
-            </label>
+        )}
 
-            <InputImage customImageList={customImageList} isEnter={isEnter} handler={handler} />
-          </div>
-          <div className="flex gap-4">
-            <CustomButton type="submit">수정하기</CustomButton>
-            <CustomButton color="red">삭제하기</CustomButton>
-          </div>
-        </form>
-      ) : (
-        <p>선택한 배너가 없습니다.</p>
-      )}
+        <div>
+          <label htmlFor="banner_image" className="block mb-2 font-semibold">
+            이미지
+          </label>
+          <InputImage customImageList={customImageList} isEnter={isEnter} handler={handler} />
+        </div>
+        <div className="flex gap-4">
+          <CustomButton type="submit">수정하기</CustomButton>
+          <CustomButton type="button" color="red">
+            삭제하기
+          </CustomButton>
+        </div>
+      </form>
     </>
   );
 };
--- src/app/admin/promotion/ManageBanner.tsx
+++ src/app/admin/promotion/ManageBanner.tsx
@@ -21,19 +21,16 @@ const ManageBanner = () => {
   const [isCreateMode, setIsCreateMode] = useState<boolean>(false);
 
   return (
-    <Section title={"Banner 관리"} isCenter={false}>
+    <Section title={isCreateMode ? "Banner 생성" : "Banner 관리"} isCenter={false}>
       <div className="flex gap-4 mb-[1rem]">
-        <CustomButton disabled={isCreateMode} size="sm" onClick={() => setIsCreateMode(!isCreateMode)}>
-          생성
-        </CustomButton>
-        <CustomButton disabled={!isCreateMode} size="sm" onClick={() => setIsCreateMode(!isCreateMode)}>
-          관리
+        <CustomButton size="sm" onClick={() => setIsCreateMode(!isCreateMode)}>
+          {!isCreateMode ? "Banner 생성하기" : "Banner 관리하기"}
         </CustomButton>
       </div>
 
       {isCreateMode ? (
         <Article title={"배너 리스트"}>
-          <BannerForm />
+          <BannerForm isCreateMode={isCreateMode} />
         </Article>
       ) : (
         <>
@@ -41,7 +38,7 @@ const ManageBanner = () => {
             <BannerInfoTable bannerList={bannerList} setSelectBanner={setSelectBanner} />
           </Article>
           <Article title={"선택한 배너 수정"}>
-            <BannerForm selectBanner={selectBanner} />
+            <BannerForm selectBanner={selectBanner} isCreateMode={isCreateMode} />
           </Article>
         </>
       )}
