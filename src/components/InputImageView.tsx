362100783a144768495b9afe2226cd0c4edfe7f5
dosion9 <dosion9ood@gmail.com>
2024-02-04 20:49:02 +0900

    fix : size

--- src/components/InputImageView.tsx
+++ src/components/InputImageView.tsx
@@ -12,13 +12,13 @@ const InputImageView = ({ customImageList, handleDeleteImageClick }: Props) => {
     <div className="flex gap-[15px] min-h-[118px] flex-wrap ">
       {customImageList?.map((n, i) => {
         return (
-          <figure key={i} draggable="true" className="w-[354px] h-[118px] overflow-hidden bg-gray-100">
+          <figure key={i} draggable="true" className="w-[118px] h-[118px] overflow-hidden bg-gray-100">
             <Image
               src={n.previewURL}
               alt=""
               className="w-auto h-[118px] mx-auto border border-line rounded-[5px]"
-              width={1920}
-              height={640}
+              width={118}
+              height={118}
               onClick={(e) => handleDeleteImageClick(e, n)}
             />
           </figure>
