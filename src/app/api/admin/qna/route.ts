54e010eb8d5e8753876aef49e3c69d39ba0c4e8c
peeChulchul <eyeless356@naver.com>
2024-02-04 14:46:22 +0900

    fix:전체 카테고리조회시 답변완료 여부 체크안되던부분 수정

--- src/app/api/admin/qna/route.ts
+++ src/app/api/admin/qna/route.ts
@@ -5,7 +5,6 @@ export const GET = async (req: NextRequest, context: { params: { productId: stri
   const searchParams = req.nextUrl.searchParams;
 
   const ANSWER = searchParams.get("answer");
-  const CATEGORY = searchParams.get("category");
   const PAGE = searchParams.get("page") || 1;
   const page = Number(PAGE);
 
@@ -13,7 +12,34 @@ export const GET = async (req: NextRequest, context: { params: { productId: stri
   const min = (page - 1) * limit;
   const max = page * limit - 1;
 
-  let { count } = await supabase.from("qna").select("product!inner(category_id)", { count: "exact", head: true });
+  if (ANSWER === "답변완료") {
+    let { count } = await supabase
+      .from("qna")
+      .select("product!inner(category_id)", { count: "exact", head: true })
+      .not("answer", "is", null);
+
+    const maxPage = Math.ceil(Number(count) / limit);
+    const nextPage = page === maxPage ? null : page + 1;
+    const prevPage = page === 1 ? null : page - 1;
+
+    let { data: qna, error } = await supabase
+      .from("qna")
+      .select("*,userinfo!inner(username,avatar_url,id),product!inner(thumbnail,name)")
+      .limit(limit)
+      .not("answer", "is", null)
+      .order("created_at", { ascending: false })
+      .range(min, max);
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ qna, maxPage, nextPage, prevPage });
+  }
+
+  let { count } = await supabase
+    .from("qna")
+    .select("product!inner(category_id)", { count: "exact", head: true })
+    .is("answer", null);
 
   const maxPage = Math.ceil(Number(count) / limit);
   const nextPage = page === maxPage ? null : page + 1;
@@ -23,6 +49,7 @@ export const GET = async (req: NextRequest, context: { params: { productId: stri
     .from("qna")
     .select("*,userinfo!inner(username,avatar_url,id),product!inner(thumbnail,name)")
     .limit(limit)
+    .is("answer", null)
     .order("created_at", { ascending: false })
     .range(min, max);
 
