03927d065c324d59db9aa6dc8cf156c91e484446
peeChulchul <eyeless356@naver.com>
2024-02-05 20:25:00 +0900

    fix:리뷰에도 제목이 나오도록 수정

--- src/components/review/ReviewItem.tsx
+++ src/components/review/ReviewItem.tsx
@@ -74,8 +74,7 @@ const ReviewItem = ({ review, currentUserId, listType, productId, isMypage }: Pr
         </div>
 
         <div>
-          {listType === "qna" && <h1 className="font-medium text-[17px] text-tc-base pb-[10px]">{review.title}</h1>}
-
+          <h1 className="font-medium text-[17px] text-tc-base pb-[10px]">{review.title}</h1>
           <p className="font-medium text-[15px] text-tc-middle leading-normal">{review.content}</p>
         </div>
 
